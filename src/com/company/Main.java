746a6217e202b7ceec3a5912c1ed071366016502
Andrew Lubinger <45952290+amlubinger@users.noreply.github.com>
2020-11-01 22:37:44 -0500

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/*
+ * Simple ADFGVX cipher solver written in Java.
+ *
+ * Authors: Andrew Lubinger and Amy Niu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Program entry point.
+    }
+}
